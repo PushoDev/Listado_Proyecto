faae1920f4ae29653c1a09c9f0c5a74632a3b843
Pusho.Dev <bethocubans1990@gmail.com>
2023-09-18 14:43:09 -0400

    Listado de Proyecto
    Gestiona tus tareas diarias

---
+++ js/script.js
@@ -0,0 +1,144 @@
+const fecha = document.querySelector('#fecha')
+const lista = document.querySelector('#lista')
+const elemento = document.querySelector('#elemento')
+const input = document.querySelector('#input')
+const botonEnter = document.querySelector('#boton-enter')
+const check = 'fa-check-circle'
+const uncheck = 'fa-circle'
+const lineThrough = 'line-through'
+let LIST
+
+let id // para que inicie en 0 cada tarea tendra un id diferente
+
+//creacion de fecha actualizada 
+
+const FECHA = new Date ()
+fecha.innerHTML = FECHA.toLocaleDateString('es-MX',{weekday: 'long', month: 'short', day:'numeric'})
+
+
+
+
+
+
+
+// funcion de agregar tarea 
+
+function agregarTarea( tarea,id,realizado,eliminado) {
+    if(eliminado) {return} // si existe eliminado es true si no es false 
+
+    const REALIZADO = realizado ? check : uncheck // si realizado es verdadero check si no uncheck
+
+    const LINE = realizado ? lineThrough : '' 
+
+    const elemento = `
+                        <li id="elemento">
+                        <i class="far ${REALIZADO}" data="realizado" id="${id}"></i>
+                        <p class="text ${LINE}">${tarea}</p>
+                        <i class="fas fa-trash de" data="eliminado" id="${id}"></i> 
+                        </li>
+                    `
+    lista.insertAdjacentHTML("beforeend",elemento)
+
+}
+
+
+// funcion de Tarea Realizada 
+
+function tareaRealizada(element) {
+    element.classList.toggle(check)
+    element.classList.toggle(uncheck)
+    element.parentNode.querySelector('.text').classList.toggle(lineThrough)
+    LIST[element.id].realizado = LIST[element.id].realizado ?false :true //Si
+   // console.log(LIST)
+   // console.log(LIST[element.id])
+   // console.log(LIST[element.id].realizado)
+}
+
+function tareaEliminada(element){
+   // console.log(element.parentNode)
+   // console.log(element.parentNode.parentNode)
+    element.parentNode.parentNode.removeChild(element.parentNode)
+    LIST[element.id].eliminado = true
+    console.log(LIST)
+}
+
+
+
+
+
+// crear un evento para escuchar el enter y para habilitar el boton 
+
+botonEnter.addEventListener('click', ()=> {
+    const tarea = input.value
+    if(tarea){
+        agregarTarea(tarea,id,false,false)
+        LIST.push({
+            nombre : tarea,
+            id : id,
+            realizado : false,
+            eliminado : false
+        })
+        localStorage.setItem('TODO',JSON.stringify(LIST))
+        id++
+        input.value = ''
+    }
+
+})
+
+document.addEventListener('keyup', function (event) {
+    if (event.key=='Enter'){
+        const tarea = input.value
+        if(tarea) {
+            agregarTarea(tarea,id,false,false)
+        LIST.push({
+            nombre : tarea,
+            id : id,
+            realizado : false,
+            eliminado : false
+        })
+        localStorage.setItem('TODO',JSON.stringify(LIST))
+     
+        input.value = ''
+        id++
+        console.log(LIST)
+        }
+    }
+    
+})
+
+
+lista.addEventListener('click',function(event){
+    const element = event.target 
+    const elementData = element.attributes.data.value
+    console.log(elementData)
+    
+    if(elementData == 'realizado') {
+        tareaRealizada(element)
+    }
+    else if(elementData == 'eliminado') {
+        tareaEliminada(element)
+        console.log("elimnado")
+    }
+    localStorage.setItem('TODO',JSON.stringify(LIST))
+})
+
+
+
+
+let data = localStorage.getItem('TODO')
+if(data){
+    LIST = JSON.parse(data)
+    console.log(LIST)
+    id = LIST.length
+    cargarLista(LIST)
+}else {
+    LIST = []
+    id = 0
+}
+
+
+function cargarLista(array) {
+    array.forEach(function(item){
+        agregarTarea(item.nombre,item.id,item.realizado,item.eliminado)
+    })
+}
---
+++ readme.md
@@ -0,0 +1,35 @@
+# Aplicación de Lista de Tareas
+
+Esta es una aplicación web de ejemplo básico que te permite crear y administrar tus tareas diarias. Puedes utilizarla para llevar un registro de las tareas que debes completar y marcarlas como completadas una vez que las hayas terminado. La aplicación está construida utilizando tecnologías web como HTML, CSS y JavaScript.
+
+![Vista previa de la aplicación](./img/listado.png)
+
+## Instalación
+
+1. Clona este repositorio en tu máquina local.
+2. Abre el archivo `index.html` en tu navegador web.
+
+## Uso
+
+1. Escribe el nombre de una tarea en el campo de texto y presiona Enter o haz clic en el botón "Agregar" para añadir la tarea a la lista.
+2. Una vez que hayas completado una tarea, marca la casilla de verificación junto a la tarea para marcarla como completada.
+3. Si deseas eliminar una tarea de la lista, haz clic en el botón "Eliminar" junto a la tarea correspondiente.
+
+## Contribución
+
+Si deseas contribuir a este proyecto, sigue los pasos a continuación:
+
+1. Realiza un fork de este repositorio.
+2. Crea una rama nueva con el nombre de tu función o mejora.
+3. Realiza los cambios necesarios y realiza commits con mensajes descriptivos.
+4. Envía un pull request indicando los cambios que has realizado.
+
+## Licencia
+
+Este proyecto es esta hecho con fines educativos. Puedes consultar el archivo [LICENSE](LICENSE) para obtener más información.
+
+## Contacto
+
+Si tienes alguna pregunta o sugerencia sobre este proyecto, no dudes en ponerte en contacto conmigo a través de mi dirección de correo electrónico: [bethocubans1990@gmail.com](mailto:bethocubans1990@gmail.com).
+
+¡Espero que esta aplicación de lista de tareas sea útil para ti! ¡Disfrútala y mantén tu día organizado!
